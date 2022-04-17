985f38e448fe659fe0ad8aa9c2f8e3c7009c618a
Naddaamahmoud <49621370+Naddaamahmoud@users.noreply.github.com>
2022-04-17 14:41:25 +0200

    Updated functions
    The meeting is split by '.' to be able to create a Markov model with words instead of letters

--- ServicesModule/MarkovChains.py
+++ ServicesModule/MarkovChains.py
@@ -9,7 +9,6 @@ import os
 import re
 import string
 import nltk
-nltk.download('punkt')
 from nltk.tokenize import word_tokenize
 from nltk.corpus import stopwords
 import random
@@ -23,13 +22,16 @@ def read_file(file_path):
     txt = my_file.readlines()
     return txt
 
-req = read_file(file_path)
-print("number of lines = ", len(req))
+#req = read_file(file_path)
+#print("number of lines = ", len(req))
 
 
-def clean_txt(txt):
+def clean_txt(txtf):
     cleaned_txt = []
+    txt = txtf.split(".")
+    print(txt)
     for line in txt:
+        print(line)
         line = line.lower()
         line = re.sub(r"[,.\"\'!@#$%^&*(){}?/;`~:<>+=-\\]", "", line)
         tokens = word_tokenize(line)
@@ -38,8 +40,8 @@ def clean_txt(txt):
     return cleaned_txt
 
 
-cleaned_req = clean_txt(req)
-print("number of words = ", len(cleaned_req))
+#cleaned_req = clean_txt(req)
+#print("number of words = ", len(cleaned_req))
 
 
 def make_markov_model(cleaned_stories, n_gram=2):
@@ -69,11 +71,11 @@ def make_markov_model(cleaned_stories, n_gram=2):
     return markov_model
 
 
-markov_model = make_markov_model(cleaned_req)
-print("number of states = ", len(markov_model.keys()))
+#markov_model = make_markov_model(cleaned_req)
+#print("number of states = ", len(markov_model.keys()))
 
-print("All possible transitions from 'product shall' state: \n")
-print(markov_model['product shall'])
+#print("All possible transitions from 'product shall' state: \n")
+#print(markov_model['product shall'])
 
 
 def generate_req(markov_model, limit=100, start='the'):
@@ -95,12 +97,12 @@ def generate_req(markov_model, limit=100, start='the'):
  #   print(str(i)+". ", generate_req(markov_model, start="product shall", limit=12))
 
 
-meet="The product shall interface with the Choice Parts System.  This provides the feed of recycled parts data.The product shall run on the existing hardware for all environments the product shall adhere to the corporate Architecture guidelines The product shall comply with corporate User Interface Guidelines The product shall comply with corporate color scheme The appearance of the product shall appear professional The software product is expected to run on Windows or Linux platforms. The product shall be easy to use by Adjusters and Collision Estimators.  95% of Adjusters and Collision Estimators shall find the product easy to use. The product shall increase productivity of Collision Estimators. 80% of the Collision Estimators shall agree their productivity has increase within 1 month of using the product. Users shall feel satisfied using the product.  85% of all users will be satisfied with the product. The product shall be easy to learn by Adjusters and Collision Estimators.  The product shall be learned with two days onsite training The user shall easily locate instructions while using the product.  User help can be found within 90% of the system."
+meet="Good Morning News. How are you doing? How are you soon? It's our first from many more meetings are fun and beneficial. Okay. Can I record the meetings? Of course? OK, Google, can you receive describe what you need to do? I want to build a weather station, open up the phone. That's the for the tcsd community. It should be a blog sites where anyone can share their thoughts and problems. They can communicate with others to sounds great. Looks like, when registering a new account the PTSD patients should avoid his username fast with phone number gender, his birthday. The year of his drama and a brief description of his case and whether he is under a doctor's supervision or not, Yes sure. It is an essential Parts. They can also log in as guests to navigate, but not to participate in anything. Yes. He can. He just can't write a comment or enter a room. What do you mean by that? The luggage in users can enter a room based on their gender or age? Is he wants, what does a room the first from a Blog? A room? It's a private face. To people feel more free to share their thoughts. The rooms Edmonds, is the creator of the room. He can invite the admin can make their own private or public. What's the difference between a private room and the public one? A public room will be visible. So that users can request to enter the room while a private one. No one will know our room exists except that users in the room. Can I guess tentaroo know? Okay, how can a user login? Yes, a user can follow. Unfollow, hide orphan users. The application should suggest users with similar cases to login users. In order to connect a user. Can also write whatever he wants in his blog, as long as it means the applications policy. What's the address for the sea? Can we discuss it in the next meeting? I didn't finish. It says you like I can say, we can see the next meeting. Sounds good. When is it going to be? Well, maybe after 2 weeks to schedule. Perfect. Thank you. Anytime. Sorry more functionality before I forget. Yes, of course. What is it? A user can choose whether he wants to be anonymous or not. Okay, that's a pretty good feature to start within the next meeting. Yes. I thought so too. Okay. Thank you. Goodbye. See you soon. "
 
 
 def red(req,seed,l):
     req_arr=[]
-    cm = clean_txt(read_file(file_path))
+    cm = clean_txt(req)
     print('clean text: ',cm)
     mm = make_markov_model(cm)
     print('model: ',mm)
@@ -109,8 +111,8 @@ def red(req,seed,l):
     return req_arr
 
 
-arr=red(meet,seed="product shall",l=12)
-for i in range(10):
+arr=red(meet,seed="user can",l=10)
+for i in range(5):
     print(arr[i])
 
 def print_hi(name):
