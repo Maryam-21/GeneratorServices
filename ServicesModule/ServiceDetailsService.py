937f5af1080025c482800781316c71eab642300f
Maryam-21 <58235353+Maryam-21@users.noreply.github.com>
2022-04-17 17:51:27 +0200

    minor code refactoring

--- ServicesModule/ServiceDetailsService.py
+++ ServicesModule/ServiceDetailsService.py
@@ -3,5 +3,5 @@ import ServicesModule.MarkovChains as MC
 import PreprocessingModule.discourse as disc
 def do(meetingscript,actors):
     editedmeetingscript = disc.main(meetingscript)
-    #text = MC.get(editedmeetingscript,actors) #meeting script #actors
+    text = MC.get(editedmeetingscript,actors) #meeting script #actors
     return IE.getServices()
