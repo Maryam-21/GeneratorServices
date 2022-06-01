5683899cc72d2a6020c487fa482e833b4b8c308f
Maryam-21 <58235353+Maryam-21@users.noreply.github.com>
2022-06-01 18:24:25 +0200

    refactor

--- ServicesModule/ServiceDetailsService.py
+++ ServicesModule/ServiceDetailsService.py
@@ -4,8 +4,9 @@ import ServicesModule.MarkovChains as MC
 import ServicesModule.AssignTimestamps as AT
 def do(meetingscript, actors, timeStamps):
     #editedmeetingscript = disc.main(meetingscript)
+    #print(meetingscript)
     sentences = MC.services(meetingscript) #meeting script #actors
     print(sentences)
     serviceDetails = IE.getServices(sentences)
     serviceDetailsStamped = AT.AssignTimestamps(serviceDetails, timeStamps)
-    return serviceDetailsStamped
+    return sentences
